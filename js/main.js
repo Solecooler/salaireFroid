c12bb21d3e47ff1b927bddd62c2a97bb9c357095
D3vcode88 <d3vcode@protonmail.com>
2024-10-02 16:29:38 +0200

    modification du visuel

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const inputElement = document.getElementById('initial-value');
+    const resultElement = document.getElementById('result-value');
+  
+    inputElement.addEventListener('input', calculateResult);
+  
+    function calculateResult() {
+      const initialValue = parseFloat(inputElement.value) || 0;
+      const result = initialValue * 1.1; // Augmentation de 10%
+      resultElement.textContent = result.toFixed(2);
+    }
+  });
