162a4d961cad903cde9d1be2b7b26c3879247c0b
D3vcode88 <d3vcode@protonmail.com>
2024-10-03 11:53:37 +0200

    modification psoition input & changement valeur & fixed a 1

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
     inputElement.addEventListener('input', calculateResult);
   
     function calculateResult() {
-      const initialValue = parseFloat(inputElement.value);
+      const initialValue = parseFloat(inputElement.value) || 0;
       const result = initialValue * 1.1; // Augmentation de 10%
       resultElement.textContent = result.toFixed(1);
     }
+    resultElement.innerText = "0";
   });
